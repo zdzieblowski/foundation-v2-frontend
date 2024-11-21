df6775ade3413605e1a646d81ba3d2ecdad567df
Piotr Zdzieblowski <piotr@zdzieblowski.com>
2024-11-21 20:31:23 +0000

    small css refactor

--- src/pages/home.php
+++ src/pages/home.php
@@ -3,26 +3,26 @@
   $metadata_current = getData('http://localhost:3001/api/v2/evrmore/current/metadata');
   $network_current = getData('http://localhost:3001/api/v2/evrmore/current/network');
 ?>
-<div class="home_three_columns">
-  <div class="home_div bg_orange">
+<div class="three_columns">
+  <div class="box bg_orange">
     <div>Currency</div>
     <div class="text_large"><?php echo $server_configuration['name']; ?></div>
   </div>
-  <div class="home_div bg_lightgrey">
+  <div class="box bg_lightgrey">
     <div>Symbol</div>
     <div class="text_large"><?php echo $frontend_configuration['pool_currency_symbol']; ?></div>
   </div>
-  <div class="home_div bg_lightgrey">
+  <div class="box bg_lightgrey">
     <div>Algorithm</div>
     <div class="text_large"><?php echo $server_configuration['algorithm']; ?></div>
   </div>
 </div>
-<div class="home_two_columns" style="margin-top: 8px;">
-  <div class="home_div bg_darkgrey">
+<div class="two_columns" style="margin-top: 8px;">
+  <div class="box bg_darkgrey">
     <div>Minimal payout</div>
     <div class="text_large"><?php echo $server_configuration['minimumPayment'].$frontend_configuration['pool_currency_symbol']; ?></div>
   </div>
-  <div class="home_div bg_darkgrey">
+  <div class="box bg_darkgrey">
     <div>Pool fee</div>
     <div class="text_large"><?php echo round($server_configuration['recipientFee']*100, $frontend_configuration['page_precision']); ?> %</div>
   </div>
@@ -40,46 +40,46 @@
 </div>
 <hr/>
 <div class="text_subheader">Pool statistics</div>
-<div class="home_three_columns">
-  <div class="home_div bg_darkgrey">
+<div class="three_columns">
+  <div class="box bg_darkgrey">
     <div>Blocks mined</div>
     <div class="text_large"><?php echo $metadata_current[0]['blocks']; ?></div>
   </div>
-  <div class="home_div bg_lightgrey">
+  <div class="box bg_lightgrey">
     <div>Effort</div>
     <div class="text_large"><?php echo round($metadata_current[0]['effort'], $frontend_configuration['page_precision']); ?> %</div>
   </div>
-  <div class="home_div bg_lightgrey">
+  <div class="box bg_lightgrey">
     <div>Efficiency</div>
     <div class="text_large"><?php echo round($metadata_current[0]['efficiency'], $frontend_configuration['page_precision']); ?> %</div>
   </div>
 </div>
-<div class="home_three_columns" style="margin-top: 8px;">
-  <div class="home_div bg_darkgrey">
+<div class="three_columns" style="margin-top: 8px;">
+  <div class="box bg_darkgrey">
     <div>Hashrate</div>
     <div class="text_large"><?php echo formatLargeNumbers(round($metadata_current[0]['hashrate'], $frontend_configuration['page_precision'])).$frontend_configuration['pool_hashrate_unit']; ?></div>
   </div>
-  <div class="home_div bg_lightgrey">
+  <div class="box bg_lightgrey">
     <div>Miners</div>
     <div class="text_large"><?php echo $metadata_current[0]['miners']; ?></div>
   </div>
-  <div class="home_div bg_lightgrey">
+  <div class="box bg_lightgrey">
     <div>Workers</div>
     <div class="text_large"><?php echo $metadata_current[0]['workers']; ?></div>
   </div>
 </div>
 <hr/>
 <div class="text_subheader">Network statistics</div>
-<div class="home_three_columns">
-  <div class="home_div bg_lightgrey">
+<div class="three_columns">
+  <div class="box bg_lightgrey">
     <div>Hashrate</div>
     <div class="text_large"><?php echo formatLargeNumbers(round($network_current[0]['hashrate'], $frontend_configuration['page_precision'])).$frontend_configuration['pool_hashrate_unit']; ?></div>
   </div>
-  <div class="home_div bg_lightgrey">
+  <div class="box bg_lightgrey">
     <div>Block height</div>
     <div class="text_large"><?php echo $network_current[0]['height']; ?></div>
   </div>
-  <div class="home_div bg_lightgrey">
+  <div class="box bg_lightgrey">
     <div>Difficulty</div>
     <div class="text_large"><?php echo round($network_current[0]['difficulty'], $frontend_configuration['page_precision']); ?></div>
   </div>
