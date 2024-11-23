06194e10aea3419776222022a169b4bdff8fe62d
Piotr Zdzieblowski <piotr@zdzieblowski.com>
2024-11-23 00:59:48 +0000

    js methods update

--- src/common/methods.js
+++ src/common/methods.js
@@ -2,4 +2,4 @@ function revealContent(elementId) {
   if (document.getElementById(elementId).classList.contains('hidden')) {
     document.getElementById(elementId).classList.toggle('shown');
   }
-};
+}
