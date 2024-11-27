71461f4831c319c5cdd03459f8b44b2a12b2c14b
Piotr Zdzieblowski <piotr@zdzieblowski.com>
2024-11-27 02:56:42 +0000

    v0.1.3

--- src/common/configuration.php
+++ src/common/configuration.php
@@ -1,5 +1,4 @@
 <?php
-$server_configuration = getData('http://localhost:3001/api/v2/evrmore/current/configuration')[0];
 
 $frontend_configuration = [
   'page_title' => 'The Mining Site: Evrmore',
@@ -10,10 +9,14 @@ $frontend_configuration = [
 
   'math_precision' => 2,
 
+  'pool_ip' => '5.75.235.4',
   'pool_name' => 'evrmore',
   'pool_hashrate_unit' => 'H/s',
   'pool_donation_currency' => 'Evrmore',
   'pool_donation_wallet' => 'EKD6T8CKqTmebBFnpjmHFRCY2jWs117wPS',
   'pool_donation_explorer_link' => 'https://evr.cryptoscope.io/address/?address=EKD6T8CKqTmebBFnpjmHFRCY2jWs117wPS'
 ];
+
+$server_configuration = getData('http://'.$frontend_configuration['pool_ip'].':3001/api/v2/evrmore/current/configuration')[0];
+
 ?>
--- src/pages/blocks.php
+++ src/pages/blocks.php
@@ -1,6 +1,6 @@
 <?php
-$blocks_combined = getData('http://localhost:3001/api/v2/evrmore/combined/blocks');
-$rounds_combined = getData('http://localhost:3001/api/v2/evrmore/combined/rounds');
+$blocks_combined = getData('http://'.$frontend_configuration['pool_ip'].':3001/api/v2/evrmore/combined/blocks');
+$rounds_combined = getData('http://'.$frontend_configuration['pool_ip'].':3001/api/v2/evrmore/combined/rounds');
 ?>
 <div class="text_header">Blocks</div>
 <div class="text_normal">List of mined blocks and rounds.</div>
