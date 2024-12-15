3ba3e4f24aebb6a10492b575023cdd4ca1b42e6b
Piotr Zdzieblowski <piotr@zdzieblowski.com>
2024-12-15 00:50:49 +0000

    pool index automatization

--- src/EVR/index.php
+++ src/EVR/index.php
@@ -1,4 +1,4 @@
-<?php 
-$pool ='EVR';
-require_once('../_frontend/index.php');
+<?php
+  $pool = basename(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
+  require_once('../_frontend/index.php');
 ?>
--- src/GRLC/index.php
+++ src/GRLC/index.php
@@ -1,4 +1,4 @@
-<?php 
-$pool ='GRLC';
-require_once('../_frontend/index.php');
+<?php
+  $pool = basename(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
+  require_once('../_frontend/index.php');
 ?>
