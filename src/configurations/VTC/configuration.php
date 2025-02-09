d6fdc1afdd4f3166b76aa4c83df531047966150b
Piotr Zdzieblowski <piotr@zdzieblowski.com>
2025-02-09 02:24:58 +0100

    0.4.6 update 15

---
+++ src/configurations/VTC/configuration.php
@@ -0,0 +1,36 @@
+<?php
+
+$pool_configuration = [
+  'title' => 'Vertcoin',
+
+  'math_precision' => 2,
+
+  'color' => '#198652',
+  'donation_currency' => 'Vertcoin',
+
+  'donation_wallet' => 'vtc1qad0dxqkx6y366rgvarqlm5emcjvr68akgtzkl2',
+  'donation_explorer_link' => 'https://chainz.cryptoid.info/vtc/search.dws?q=vtc1qad0dxqkx6y366rgvarqlm5emcjvr68akgtzkl2',
+
+  'network_difficulty_multiplier' => 1,
+  'network_hashrate_multiplier' => 1,
+  'hashrate_unit' => 'H/s',
+  'ip' => '127.0.0.1',
+  'port' => '2006',
+
+  'name' => 'vertcoin',
+
+  'suggested_platform_gpu' => True,
+  'suggested_platform_asic' => False,
+  'suggested_software' => 'WildRig Multi',
+  'suggested_software_linux' => 'wildrig-multi',
+  'suggested_software_windows' => 'wildrig.exe',
+  'suggested_command_algo' => ' -a ',
+  'suggested_command_open' => ' -o ',
+  'suggested_command_wallet' => ' -u ',
+  'suggested_command_worker' => ' -w ',
+  'suggested_software_link' => 'https://github.com/andru-kun/wildrig-multi/releases'
+];
+
+$server_configuration = getData('http://'.$pool_configuration['ip'].':'.$pool_configuration['port'].'/api/v2/'.$pool_configuration['name'].'/current/configuration')[0];
+
+?>
---
+++ src/configurations/ZEC/configuration.php
@@ -0,0 +1,36 @@
+<?php
+
+$pool_configuration = [
+  'title' => 'Zcash',
+
+  'math_precision' => 2,
+
+  'color' => '#f3b724',
+  'donation_currency' => 'Zcash',
+
+  'donation_wallet' => 't1cJHk4SnjFgaoNc3HrhKcSE1pFzawWmcJ5',
+  'donation_explorer_link' => 'https://mainnet.zcashexplorer.app/address/t1cJHk4SnjFgaoNc3HrhKcSE1pFzawWmcJ5',
+
+  'network_difficulty_multiplier' => 1,
+  'network_hashrate_multiplier' => 1,
+  'hashrate_unit' => 'H/s',
+  'ip' => '127.0.0.1',
+  'port' => '2007',
+
+  'name' => 'zcash',
+
+  'suggested_platform_gpu' => False,
+  'suggested_platform_asic' => True,
+  'suggested_software' => '',
+  'suggested_software_linux' => '',
+  'suggested_software_windows' => '',
+  'suggested_command_algo' => ' -a ',
+  'suggested_command_open' => ' -o ',
+  'suggested_command_wallet' => ' -u ',
+  'suggested_command_worker' => '',
+  'suggested_software_link' => ''
+];
+
+$server_configuration = getData('http://'.$pool_configuration['ip'].':'.$pool_configuration['port'].'/api/v2/'.$pool_configuration['name'].'/current/configuration')[0];
+
+?>
